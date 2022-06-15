fc58785020177224b2d2235a3bdc93e1f2528e63
Grigory <grishavinyar@icloud.com>
2022-06-15 21:56:15 +0300

    Built the project
    1. Deleted Storyboard
    2. Setup project in SceneDelegate

---
+++ Custom photos collection/Custom photos collection/CollectionVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom photos collection
+//
+//  Created by Григорий Виняр on 15/06/2022.
+//
+
+import UIKit
+
+class CollectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .purple
+    }
+
+
+}
+
